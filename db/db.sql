f159e53e045a49a7a47621bbdfa454c3d9fd718e
Miguel Angel Sánchez Contreras <mskodesolutions@gmail.com>
2021-11-04 23:08:53 -0500

    info get method

---
+++ db/db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS company;
+USE company;
+CREATE TABLE employees (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) DEFAULT NULL,
+    salary INT(11) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
+DESCRIBE employees;
+INSERT INTO employees values
+    (1, 'Ryan Ray', 20000),
+    (2, 'Joe McMilan', 45000),
+    (3, 'John Carter', 75000);
+    
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin';
+flush privileges;
+SELECT * FROM employees;
