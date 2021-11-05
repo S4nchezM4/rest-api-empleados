1394de9d22059b292ed67a1ce75e78c8757b7a04
Miguel Angel Sánchez Contreras <mskodesolutions@gmail.com>
2021-11-05 00:03:01 -0500

    Finishing the CRUD methods of the Rest Api

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const express = require('express');
 const router = express.Router();
 
@@ -10,19 +11,55 @@ router.get ('/', (req, res) => {
         }else {
             console.log(err);
         }
-    })
+    });
 });
 
 router.get ('/:id', (req, res) => {
     const { id } = req.params
-    mysqlConnection.query('SELECT * FROM employees WHERE id = ?', [id], (err, rows, fields) =>{
+    mysqlConnection.query('SELECT * FROM employees WHERE id = ?', [id], (err, rows, fields) => {
         if(!err){
             res.json(rows[0]);
         }else{
             console.log(err);
         }
-    })
-})
+    });
+});
+router.post ('/', (req, res) => {
+    const { id, name, salary } = req.body //falta validar si los datos son nulos
+    const query = `
+        CALL employeeAddOrEdit(?, ?, ?);
+    `;
+    mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+        if (!err) {
+            res.json({Status: 'Employeed saved'})
+        }else {
+            console.log(err);
+        }
+    });
+});
 
+router.put ('/:id', (req, res) => {
+    const { name, salary } = req.body;
+    const { id } = req.params;
+    const query = 'CALL employeeAddOrEdit(?, ?, ?)';
+    mysqlConnection.query(query, [id, name, salary], (err, rows, filds) => {
+        if (!err){
+            res.json({status: 'Employee Updated'});
+        }else {
+            console.log(err);
+        }
+    });
+});
+
+router.delete ('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('DELETE FROM employees WHERE id = ?', [id], (err, rows, filds) => {
+        if(!err){
+            res.json({status: 'Employee Deleted'});
+        }else {
+            console.log(err);
+        }
+    });
+});
 
 module.exports = router;
